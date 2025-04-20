9ac055c71f47cf16222d01f73cdc764fe08e7008
DrozsdikAdam <drozsdik17@gmail.com>
2025-04-20 13:59:44 +0200

    oldalpanel hozzáadása és kezdetleges funkciók

--- Telefonkönyv/MainWindow.xaml.cs
+++ Telefonkönyv/MainWindow.xaml.cs
@@ -1,13 +1,34 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using static MaterialDesignThemes.Wpf.Theme;
 
 namespace Telefonkönyv
 {
+    public class PhoneBookEntry
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadPhoneBookEntries();
+        }
+
+        private void LoadPhoneBookEntries()
+        {
+            var phoneBookEntries = new List<PhoneBookEntry>
+            {
+                new PhoneBookEntry { Name = "Kiss János", City = "Budapest", Phone = "123456789" },
+                new PhoneBookEntry { Name = "Nagy Anna", City = "Debrecen", Phone = "987654321" },
+                new PhoneBookEntry { Name = "Tóth Péter", City = "Szeged", Phone = "456123789" }
+            };
+
+            PhoneBookList.ItemsSource = phoneBookEntries;
         }
 
         private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -31,6 +52,15 @@ namespace Telefonkönyv
             regWindow.ShowDialog();
         }
 
-
+        private void PhoneBookList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+               var selected = PhoneBookList.SelectedItem as PhoneBookEntry;
+            if (selected != null)
+            {
+                sideCity.Text = selected.City;
+                sideName.Text = selected.Name;
+                sideTel.Text = selected.Phone;
+            }
+        }
     }
 }
